081954bcafe4e6c5211353aa7e74ce81e605a136
polyglotDataNerd <gerardnino@gmail.com>
2020-04-23 08:10:04 -0700

    added pdf renderer pdf.js

---
+++ simple.js
@@ -0,0 +1,30 @@
+'use strict'
+const pdf = require("/viewer/build/pdf"),
+    loadingTask = pdf.getDocument("./GerardBartolome_CV2020.pdf")
+loadingTask.promise.then(
+    function (pdf) {
+        // Load information from the first page.
+        pdf.getPage(1).then(function (page) {
+            var scale = 1;
+            var viewport = page.getViewport(scale);
+
+            // Apply page dimensions to the <canvas> element.
+            var canvas = document.getElementById("pdf");
+            var context = canvas.getContext("2d");
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Render the page into the <canvas> element.
+            var renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
+            page.render(renderContext).then(function () {
+                console.log("Page rendered!");
+            });
+        });
+    },
+    function (reason) {
+        console.error(reason);
+    }
+);
